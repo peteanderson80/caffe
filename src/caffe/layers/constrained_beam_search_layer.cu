eeafa9874b50599aa69d1dae23492c648833606f
Peter Anderson <peteanderson80@gmail.com>
2018-11-21 16:59:14 -0500

    Fix minor bug in constrained beam search

--- src/caffe/layers/constrained_beam_search_layer.cu
+++ src/caffe/layers/constrained_beam_search_layer.cu
@@ -23,8 +23,8 @@ __global__ void UpdateConstrainedScore(
   const Dtype* allowed_multiple_data,
   const Dtype* sequence_output_prev,
   const Dtype* input_sequence_data,
-  const int bag_size,
-  const int num_alternatives,
+  const int num_conjunctions,
+  const int num_disjunctions,
   const Dtype* constraint_data,
   const Dtype* output_score_sorted,
   const Dtype* output_score_data,
@@ -34,8 +34,9 @@ __global__ void UpdateConstrainedScore(
   int* score_indices_data)
 {
   CUDA_KERNEL_LOOP(idx, nthreads) {
-    // One kernel for each partial sequence. Consider the top beam_size possible expansions, 
-    // and calculate the score for the resulting sequence. Note: score_data starts as -FLT_MAX.
+    // One kernel for each partial sequence. Consider the best possible expansions, 
+    // and calculate the score for the resulting sequence. Save the results in score_data
+    // with the matching indices in score_indices_data. Note: score_data starts as -FLT_MAX.
     const bool beam_complete = (timestep > 0) && (input_sequence_data[idx] == Dtype(end_of_sequence));
     const unsigned int state = (idx / beam_size) % num_states;
     const bool is_constrained = (state > 0);
@@ -44,7 +45,8 @@ __global__ void UpdateConstrainedScore(
     for (num_constraints = 0; state_copy; state_copy >>= 1){
       num_constraints += state_copy & 1;
     }
-    int max_expansions = beam_size + bag_size * num_alternatives;
+    // At most we need to consider this many expansions for this beam.
+    int max_expansions = beam_size + num_conjunctions * num_disjunctions;
 
     // Expand own partial sequence
     int c = 0; // candidate index
@@ -99,20 +101,24 @@ __global__ void UpdateConstrainedScore(
       c++;
     }
     // Expand partial sequences from other states
-    if (is_constrained && !beam_complete && timestep >= (num_constraints-1)) {
+    if (is_constrained && timestep >= (num_constraints-1)) {
       // Add further beam expansions by adding constraint words to the unconstrained beams
       const int n = idx / (num_states*beam_size); // batch index
-      for (int c=0; c<bag_size; ++c) {
+      for (int c=0; c<num_conjunctions; ++c) {
         // Check if this state is conditional on these alternatives, by checking bit in position c
         if (state & (1<<c)) {
-          for (int a=0; a<num_alternatives; ++a) {
-            int constraint_word = constraint_data[(n*bag_size+c)*num_alternatives+a];
+          for (int a=0; a<num_disjunctions; ++a) {
+            int constraint_word = constraint_data[(n*num_conjunctions+c)*num_disjunctions+a];
             if (constraint_word == end_of_sequence) {
               continue; // ignore
             }
             unsigned int src_state = state & ~(1<<c); // Src state is same but with zeroed bit c
-            int offset = idx*max_expansions + beam_size + c*num_alternatives + a;
+            int offset = idx*max_expansions + beam_size + c*num_disjunctions + a;
             int src_idx = idx-(state-src_state)*beam_size;
+            const bool src_beam_complete = (input_sequence_data[src_idx] == Dtype(end_of_sequence));
+            if (src_beam_complete) {
+              continue; // Can't add constraint words to a completed sequence
+            }
             int index = src_idx * vocab_size + constraint_word; // Source state index
             Dtype next_word_score = output_score_data[index];
             if (timestep==0 && num_constraints==1) {
@@ -127,6 +133,30 @@ __global__ void UpdateConstrainedScore(
               score_indices_data[offset] = index;
               if (prevent_repeats && input_sequence_data[src_idx] == constraint_word) {
                 score_data[offset] = Dtype(-FLT_MAX);
+              } else if (allowed_multiple_size > 0) {
+                // Don't allow repeat of any word not in allowed_multiple
+                bool reject = false;
+                for (int t=0; t<timestep; ++t) {
+                  Dtype prev_word = sequence_output_prev[src_idx*sequence_length+t];
+                  if (Dtype(constraint_word) == prev_word) {
+                    bool allowed_multiple = false;
+                    int r=0;
+                    for (; r<allowed_multiple_size; ++r) {
+                      if (Dtype(constraint_word) == allowed_multiple_data[r]) {
+                        allowed_multiple = true;
+                        break;
+                      }
+                    }
+                    if (!allowed_multiple) {
+                      reject = true;
+                    }
+                  }
+                }
+                if (reject) {
+                  score_data[offset] = Dtype(-FLT_MAX);
+                } else {
+                  score_data[offset] = next_word_score + partial_score[src_idx];
+                }
               } else {
                 score_data[offset] = next_word_score + partial_score[src_idx];
               }
@@ -165,8 +195,8 @@ void ConstrainedBeamSearchLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& 
     }
     const Dtype* sequence_output_prev = top[0]->gpu_data();
     const Dtype* constraint_data = bottom[0]->gpu_data();
-    const int bag_size = bottom[0]->shape(1);
-    const int num_alternatives = bottom[0]->shape(2);
+    const int num_conjunctions = bottom[0]->shape(1);
+    const int num_disjunctions = bottom[0]->shape(2);
     const Dtype* output_score_sorted = this->output_score_->gpu_diff();
     const Dtype* output_score_data = this->output_score_->gpu_data();
     const int* output_score_indices_data = this->output_score_indices_.gpu_data();
@@ -177,7 +207,7 @@ void ConstrainedBeamSearchLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& 
       <<<CAFFE_GET_BLOCKS(nthreads), CAFFE_CUDA_NUM_THREADS>>>(
         nthreads, timestep, this->end_of_sequence_, this->beam_size_, this->vocab_size_, this->sequence_length_, 
         this->num_states_, this->prevent_repeats_, this->allowed_multiple_size_, allowed_multiple_data, 
-        sequence_output_prev, input_sequence_data, bag_size, num_alternatives, 
+        sequence_output_prev, input_sequence_data, num_conjunctions, num_disjunctions, 
         constraint_data, output_score_sorted, output_score_data, output_score_indices_data, partial_score, 
         score_data, score_indices_data);
     CUDA_POST_KERNEL_CHECK;
@@ -187,15 +217,15 @@ void ConstrainedBeamSearchLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& 
     constraint_data = bottom[0]->cpu_data();
     std::ostringstream os1;
     os1 << "First batch item score pre-sorting at t=" << timestep << ", conditioned on ";
-    for (int l=0; l<bag_size*num_alternatives; ++l) {
+    for (int l=0; l<num_conjunctions*num_disjunctions; ++l) {
       os1 << constraint_data[l] << ", ";
     }
     LOG(INFO) << os1.str();
     score_data = this->score_.mutable_cpu_data();
     for (int l=0; l<this->beam_size_*this->num_states_; ++l) {
       std::ostringstream os;
-      for (int k=0; k<(this->beam_size_+bag_size*num_alternatives); ++k) {
-        os << std::setfill(' ') << std::setw(4) << score_data[l*(this->beam_size_+bag_size*num_alternatives)+k] << " ";
+      for (int k=0; k<(this->beam_size_+num_conjunctions*num_disjunctions); ++k) {
+        os << std::setfill(' ') << std::setw(4) << score_data[l*(this->beam_size_+num_conjunctions*num_disjunctions)+k] << " ";
       }
       LOG(INFO) << os.str();
       if ((l+1) % this->beam_size_ == 0) {
@@ -214,7 +244,7 @@ void ConstrainedBeamSearchLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& 
     constraint_data = bottom[0]->cpu_data();
     std::ostringstream os;
     os << "First batch item beams at t=" << timestep << ", conditioned on ";
-    for (int l=0; l<bag_size*num_alternatives; ++l) {
+    for (int l=0; l<num_conjunctions*num_disjunctions; ++l) {
       os << constraint_data[l] << ", ";
     }
     LOG(INFO) << os.str();
